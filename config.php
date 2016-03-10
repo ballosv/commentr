80a37b73a484f106c4449050bf59b5ceb2bb0352
bassels <frederik.borck@googlemail.com>
2016-03-10 19:36:00 +0100

    install on windows
    my local machine @ home

--- config.php
+++ config.php
@@ -10,7 +10,11 @@ define('VIEW_PATH', APP_PATH . DIRECTORY_SEPARATOR . 'views');
 define('MODEL_PATH', APP_PATH . DIRECTORY_SEPARATOR . 'models');
 
 // URL-Pfade
-define('BASE_URL', 'http://localhost:8888');
+// Standard Sven
+//define('BASE_URL', 'http://localhost:8888');
+// Standard Freddy
+define('BASE_URL', 'http://localhost:81');
+
 define('PUBLIC_URL', BASE_URL . DIRECTORY_SEPARATOR . 'public');
 //define('MODEL_PATH', APP_PATH . DIRECTORY_SEPARATOR . 'public');
 
